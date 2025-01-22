f3964a5ffeeafd87e639dedd5867e8b651a169a4
zhujiajun <zhujiajun.me@gmail.com>
2025-01-22 22:43:13 +0800

    Mobile Navigation Work

--- starter/09-Omnifood-Optimizations/js/script.js
+++ starter/09-Omnifood-Optimizations/js/script.js
@@ -2,6 +2,13 @@ const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
